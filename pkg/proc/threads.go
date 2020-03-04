02c043706924de15024ed887a9acb9e877915649
chainhelen <chainhelen@gmail.com>
2020-03-05 01:02:01 +0800

    Fix: remove `instr.IsCall` and skip some test

--- pkg/proc/threads.go
+++ pkg/proc/threads.go
@@ -399,7 +399,7 @@ func setStepIntoBreakpoint(dbp Process, text []AsmInstruction, cond ast.Expr) er
 	pc := instr.DestLoc.PC
 
 	// Skip InhibitStepInto functions for different arch.
-	if instr.IsCall() && dbp.BinInfo().Arch.InhibitStepInto(dbp.BinInfo(), pc) {
+	if dbp.BinInfo().Arch.InhibitStepInto(dbp.BinInfo(), pc) {
 		return nil
 	}
 
